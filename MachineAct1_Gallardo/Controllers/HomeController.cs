089b26122c3df843774ec01edefa0a26b8081a07
Draico <Draico@LAPTOP-P0PF2E50>
2023-09-13 00:15:50 +0800

    Add project files.

---
+++ MachineAct1_Gallardo/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using MachineAct1_Gallardo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace MachineAct1_Gallardo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Employee()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MachineAct1_Gallardo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MachineAct1_Gallardo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
